41eca2613598163e389e872998c9b00c49a7a235
José Kevin <josekvn21324@gmail.com>
2025-07-03 14:56:13 -0400

    Primeiro commint do projeto

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=20)
+class Postagem(models.Model):
+    titulo = models.CharField(max_length=255)
+    corpo = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_modificacao = models.DateTimeField(auto_now=True)
+    categorias = models.ManyToManyField('Categoria', related_name='postagem')   
+class Comentario(models.Model):
+    autor = models.CharField(max_length=60)
+    corpo = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    postagem = models.ForeignKey('Postagem', on_delete=models.CASCADE)   
---
+++ projetos/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from projetos.models import Projeto
+
+# Create your views here.
+
+def projeto_index(request):
+    projetos = Projeto.objects.all()
+    quantidade = projetos.count()
+    
+    context = {
+        'projetos': projetos,
+        'quantidade': quantidade,
+    }
+
+    return render(request, 'projetos/index.html', context)
+
+def projetos_detalhe(request, pk):
+    projeto = Projeto.objects.get(pk=pk)
+    context = {
+        'projeto': projeto
+    }
+
+    return render(request, 'projetos/detalhe.html', context)
